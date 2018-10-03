f7d141afa5c40588258abda5fbbfaefca7034801
shilinp <shilin@g.ucla.com>
2018-10-03 11:03:44 -0700

    Shift Operations
    Arithmetic left/right shift modules. Logical left/right shift modules. Inputs (a, b). Outputs (sum, zero, overflow).

---
+++ Lab1/arithmetic_shift_right.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module arithmetic_shift_right (
+input [15:0] a,
+input [15:0] b, 
+output [15:0] sum,
+output zero,
+output overflow);
+
+wire [15:0] a;
+wire [15:0] b;
+wire [15:0] sum;
+wire zero;
+wire overflow;
+
+assign sum = a >>> b;
+assign zero = sum & 16'b0;
+assign overflow = a[15] & sum[15];
+
+endmodule
