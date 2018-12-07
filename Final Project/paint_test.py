769a9b007a78f1da3b110ddf0fd33f0c52825b04
shilinp <shilin@g.ucla.edu>
2018-12-07 12:25:54 -0800

    python file changes

--- Final Project/paint_test.py
+++ Final Project/paint_test.py
@@ -1,34 +1,43 @@
 from tkinter import *
 from PIL import Image, ImageDraw
+import io
 
 width = 400
-height = 300
+height = 400
 black = (0, 0, 0)
 size = 28, 28
 
 def save():
-    filename = "image.png"
-    image1.thumbnail(size, Image.ANTIALIAS)
-    image1.save(filename)
+	global image1
+	image1.thumbnail(size, Image.ANTIALIAS)
+	image1 = image1.tobytes()
+	print(image1)
+	exit()
 
 def paint(event):
-    x1, y1 = (event.x - 5), (event.y - 5)
-    x2, y2 = (event.x + 5), (event.y + 5)
+	global cv, draw
+	x1, y1 = (event.x - 5), (event.y - 5)
+	x2, y2 = (event.x + 5), (event.y + 5)
 
-    cv.create_oval(x1, y1, x2, y2, fill='white', outline='white')
-    draw.ellipse([x1, y1, x2, y2], fill="white")
+	cv.create_oval(x1, y1, x2, y2, fill='white', outline='white')
+	draw.ellipse([x1, y1, x2, y2], fill="white")
 
-root = Tk()
+def main():
+	global image1, cv, draw
+	root = Tk()
 
-cv = Canvas(root, width=width, height=height, bg='black')
-cv.pack()
+	cv = Canvas(root, width=width, height=height, bg='black')
+	cv.pack()
 
-image1 = Image.new("L", (width, height))
-draw = ImageDraw.Draw(image1)
+	image1 = Image.new("L", (width, height))
+	draw = ImageDraw.Draw(image1)
 
-cv.pack(expand=YES, fill=BOTH)
-cv.bind("<B1-Motion>", paint)
+	cv.pack(expand=YES, fill=BOTH)
+	cv.bind("<B1-Motion>", paint)
 
-button=Button(text="save",command=save)
-button.pack()
-root.mainloop()
+	button=Button(text="save",command=save)
+	button.pack()
+	root.mainloop()
+
+if __name__=='__main__':
+    main()
